aeeafabaf72c28ef97c9e458555cc8238a897679
shogo takayama <shogo@chalkboard.me>
2018-09-28 01:44:36 +0900

    Parseの成功

--- app/services/link_analyze_service.rb
+++ app/services/link_analyze_service.rb
@@ -3,17 +3,16 @@ class LinkAnalyzeService
   BASE_LINK = 'https://www.mizuhobank.co.jp/retail/takarakuji/loto/backnumber/'.freeze
   re_mini  = Regexp.compile('loto([0-9]{4})\.html')
   re_loto6 = Regexp.compile('loto6([0-9]{4})\.html')
-  re_loto7 = Regexp.compile('detail\.html\?fromto=([0-9]{3-4}_[0-9]{3-4})&type=loto7')
-  REGEXS = [re_mini, re_loto6, re_loto7].freeze
+  variable = Regexp.compile('detail\.html\?fromto=([0-9]{1,4}_[0-9]{1,4})&type=(loto[6-7]|miniloto)')
+  REGEXS = [re_mini, re_loto6, variable].freeze
 
   def self.analyze(list)
     separate_link = []
     list.each do |link|
       ret = parse(link.sub(BASE_LINK, ''))
       if ret.present?
+        ret[:link] = link
         separate_link.push ret
-        p ret
-        p "type = #{ret[:type]}, data = #{ret[:data]}"
       else
         p "[ERROR] url = #{link}"
       end
@@ -24,11 +23,18 @@ class LinkAnalyzeService
   private
 
   def self.parse(link)
-    p link
     REGEXS.each_with_index do |re, i|
       m = re.match(link)
       next if m.nil?
-      return { type: i, data: m[1] }
+      if re == REGEXS.last
+        return { data: m[1], type: m[2] }
+      end
+      type = if i == 0
+               'miniloto'
+             else
+               'loto6'
+             end
+      return { data: m[1], type: type }
     end
     nil
   end
